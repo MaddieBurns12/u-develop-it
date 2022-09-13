98bf173b00ce3da6e06c9851ecf316cb4386d9d6
Madeline Burns <mburns6023@gmail.com>
2022-09-13 14:59:24 -0400

    get routes working through server

--- server.js
+++ server.js
@@ -20,16 +20,46 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// })
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name
+      AS party_name
+      FROM candidates
+      LEFT JOIN parties
+      ON candidates.party_id = parties.id`;
+
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidate.id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
     if (err) {
-        console.log(err);
+      res.status(400).json({ error: err.message });
+      return;
     }
-    console.log(row);
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
 // Delete a candidate
@@ -41,16 +71,16 @@ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
